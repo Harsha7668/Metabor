46861f80f5f0783defb3a012029bc875a8df8719
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-08 15:03:08 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -43,6 +43,11 @@ user_settings = {}
 # Initialize Gofile API key variable
 GOFILE_API_KEY = ""
 
+FILE_SIZE_LIMIT = 2000 * 1024 * 1024  # 2000 MB in bytes
+
+# Variables to store Google Drive folder ID
+GDRIVE_FOLDER_ID = None
+
 # Initialize global settings variables
 METADATA_ENABLED = True 
 PHOTO_ATTACH_ENABLED = True
@@ -2738,30 +2743,8 @@ async def gofile_upload(bot, msg: Message):
 
 
 
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
-# Function to authenticate Google Drive
-def authenticate_google_drive():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    return creds
 
-# Authenticate and create the Drive service
-creds = authenticate_google_drive()
-drive_service = build('drive', 'v3', credentials=creds)
-
-# Variables to store Google Drive folder ID
-GDRIVE_FOLDER_ID = None
 
 
 # Command handler for /mirror
@@ -2856,23 +2839,10 @@ async def setup_gdrive_id(bot, msg: Message):
 
 
 
-FILE_SIZE_LIMIT = 2000 * 1024 * 1024  # 2000 MB in bytes
 
 
 
-async def upload_to_google_drive(file_path, file_name, sts):
-    file_metadata = {'name': file_name}
-    media = MediaFileUpload(file_path, resumable=True)
-    request = drive_service.files().create(body=file_metadata, media_body=media, fields='id, webViewLink')
 
-    response = None
-    start_time = time.time()
-    while response is None:
-        status, response = request.next_chunk()
-        if status:
-            await progress_message(status.resumable_progress, os.path.getsize(file_path), "Uploading to Google Drive", sts, start_time)
-
-    return response.get('webViewLink')
 
 
 
