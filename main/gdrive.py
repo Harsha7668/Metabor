e7e1bd2f49a0d079704cb8c0c01b3c1ca4ac8d71
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-08 15:01:42 +0530

    Update gdrive.py

--- main/gdrive.py
+++ main/gdrive.py
@@ -1,9 +1,12 @@
 import os
+import time
 import pickle
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from main.utils import progress_message
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
@@ -26,3 +29,18 @@ def authenticate_google_drive():
 # Authenticate and create the Drive service
 creds = authenticate_google_drive()
 drive_service = build('drive', 'v3', credentials=creds)
+
+async def upload_to_google_drive(file_path, file_name, sts):
+    file_metadata = {'name': file_name}
+    media = MediaFileUpload(file_path, resumable=True)
+    request = drive_service.files().create(body=file_metadata, media_body=media, fields='id, webViewLink')
+
+    response = None
+    start_time = time.time()
+    while response is None:
+        status, response = request.next_chunk()
+        if status:
+            await progress_message(status.resumable_progress, os.path.getsize(file_path), "Uploading to Google Drive", sts, start_time)
+
+    return response.get('webViewLink')
+
