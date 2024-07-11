1da2ce95ea081cbd03cfc25a59474ba59b34d507
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2024-07-11 18:34:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = int(os.environ.get("API_ID", "10811400"))
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAEUGww1KdtlQsMFERlA7elYlR82U-Nb6kk")
 ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
 FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
 FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
