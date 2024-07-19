19b3481f43d4260607177064e9e8c515b0a764f6
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2024-07-19 10:29:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,4 +19,6 @@ CAPTION = os.environ.get("CAPTION", "")
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
 AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+PORT = int(os.environ.get("PORT", "8080"))
 
