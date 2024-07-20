2d36c81c36d0118a4ce999e5bfc18305cea02e7f
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2024-07-20 18:09:27 +0530

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -5,7 +5,7 @@ from pymongo.errors import PyMongoError
 from datetime import datetime
 
 class Database:
-        def __init__(self, uri, database_name):
+    def __init__(self, uri, database_name):        
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.users_col = self.db["users"]
