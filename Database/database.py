4d06a6ef71304ab45e742f2b0a21c10415d382bc
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-07-22 15:21:58 +0530

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -18,7 +18,7 @@ class Database:
         self.processes_col = self.db['processes']
 
 
-    
+        
     async def create_process(self, user_id):
         result = await self.processes_col.insert_one({'user_id': user_id, 'status': 'ongoing', 'progress': 0})
         return result.inserted_id
@@ -28,6 +28,10 @@ class Database:
 
     async def update_process(self, process_id, update_data):
         await self.processes_col.update_one({'_id': process_id}, {'$set': update_data})
+
+    async def cancel_process(self, process_id):
+        await self.update_process(process_id, {'status': 'cancelled'})
+    
     
         
     async def add_user(self, user_id: int, username: str):
