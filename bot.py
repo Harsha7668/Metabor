78c8247688274b88593f09257bb6fcb32f1bd18c
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-08-24 11:14:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ from config import *
 import os
 
 
+    
+
+
+"""
+
 class Bot(Client):    
 
     def __init__(self):
@@ -15,6 +20,38 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
+            workers=100,
+            plugins={"root": "main"},
+            sleep_threshold=10,
+        )
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()        
+        await self.send_message(GROUP, f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
+        
+                
+        
+    async def stop(self, *args):
+       await super().stop()      
+       print("Bot Restarting........")
+
+bot = Bot()
+bot.run()
+"""
+
+class Bot(Client):    
+
+    def __init__(self):
+        super().__init__(
+            name="MetaMorpher",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            session_string=STRING_SESSION,
             workers=100,
             plugins={"root": "main"},
             sleep_threshold=10,
